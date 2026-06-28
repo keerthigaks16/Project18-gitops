afbb0e6705d607809640896330fcfd427d74e6f4
keerthigaks16 <keerthigaks16@gmail.com>
2026-06-28 15:51:08 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+  }
+}
+
+provider "null" {}
+
+resource "null_resource" "demo" {
+  triggers = {
+    version = "1"
+  }
+}
